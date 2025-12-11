e357e09549c7088896c75153df8433fbdbae1349
Fahd Mohamed <164559895+Fahd-500@users.noreply.github.com>
2025-12-11 19:32:17 +0200

    Update pb_pvKEYS.py
    Removed comments explaining file handling and key saving.

--- RSAencryption/pb_pvKEYS.py
+++ RSAencryption/pb_pvKEYS.py
@@ -6,16 +6,10 @@ public_key, private_key = rsa.newkeys(1024)
 
 
 # This line opens/creates a file called public.pem
-# in write-binary mode
-# This specifies that the file is opened in binary mode, 
-# which means that data will be written as bytes, not as plain text.
 with open("public.pem", "wb") as f:
 
-    
 #public_key.save_pkcs1("PEM") converts the RSA public key to a PKCS#1 format, which structures the key in a standardized way.
 #Then, it encodes this PKCS#1-formatted key into PEM, a readable text format
-#Finally, f.write(...) writes this PEM-encoded text to the file, 
-#allowing you to save and later share the key in a commonly used format
     f.write(public_key.save_pkcs1("PEM"))
 
 
